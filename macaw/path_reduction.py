657097193cacfe9f86e5541570eeda8f14960067
Kyle Xiao <kylepxiao@gmail.com>
2017-10-26 23:22:50 -0400

    added illegal move 3

--- macaw/path_reduction.py
+++ macaw/path_reduction.py
@@ -1,3 +1,5 @@
+#TODO switch all arrays to "numpy" arrays
+
 class PathReduction(object):
     """An representation of topological paths that can be reduced
 
@@ -37,7 +39,7 @@ class PathReduction(object):
             return 1
         elif s == "++" or s == "--":
             return 2
-        elif s == "C" or s == "A":
+        elif s == "L" or s == "R":
             return 3
         else:
             return 0
@@ -64,6 +66,23 @@ class PathReduction(object):
 
         """
 
+    def __three(self, start, end):
+        """
+        Reduces illegal path beginning from starting point, going to the cyclic predecessors,
+        circles around that point, and then returns
+
+        INPUT:
+        - ``start`` -- the starting index inclusive of the illegal move
+        - ``end`` -- the ending index exclusive of the illegal move
+        """
+        c1 = self.path[start]
+        t2 = self.path[start + 4]
+        t1 = 'L' if t2 == 'R' else 'R'
+        T1 = 'RL' if t2 == 'R' else 'LR'
+        self.path[start:end] = []
+        opath = [c1, t1, c1, T1, c1]
+        self.path[start:start] = opath
+
 
     def __four(self, start, end, orientation):
         """
@@ -76,7 +95,7 @@ class PathReduction(object):
         INPUT:
         - ``start`` -- the starting index inclusive of the illegal move
         - ``end`` -- the ending index exclusive of the illegal move
-        - ``orientation`` -- string representation ("CC" or "C") of the orientation of the pants curve
+        - ``orientation`` -- string representation ("CC" or "L") of the orientation of the pants curve
                             that the illegal path is starting from
 
         EXAMPLE
@@ -86,11 +105,11 @@ class PathReduction(object):
         >> Path with form
         """
         boundary = ""
-        if orientation == "C":
+        if orientation == "L":
             boundary = "++"
         else:
             boundary = "--"
-        self.path = self.path[:start] + [boundary, "C"] + self.path[end:]
+        self.path = self.path[:start] + [boundary, "L"] + self.path[end:]
 
     def __five(self, start, end, orientation):
         """
@@ -101,11 +120,11 @@ class PathReduction(object):
         INPUT:
         - ``start`` -- the starting index inclusive of the illegal move
         - ``end`` -- the ending index exclusive of the illegal move
-        - ``orientation`` -- string representation ("CC" or "C") of the orientation of the pants curve
+        - ``orientation`` -- string representation ("CC" or "L") of the orientation of the pants curve
                             that the illegal path is starting from
         """
         boundary = ""
-        if orientation == "C":
+        if orientation == "L":
             boundary = "--"
         else:
             boundary = "++"
@@ -137,7 +156,7 @@ class PathReduction(object):
             self.path[start+1:start+6] = []
             self.path.insert(start+1, s1)
             self.path.insert(start+1, self.path[start])
-            if t1 == "C":
+            if t1 == "L":
                 self.path.insert(start+1, "--")
             else:
                 self.path.insert(start+1, "++")
@@ -145,7 +164,7 @@ class PathReduction(object):
             s1 = self.path[start+6]
             t1 = self.path[start+2]
             self.path[start+2:start+7] = []
-            if t1 == "C":
+            if t1 == "L":
                 self.path.insert(start+2, "--")
             else:
                 self.path.insert(start+2, "++")
