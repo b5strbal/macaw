48218f99cfd944ddf622549c1fa96691f0ffc9b3
Jonathan Chen <jc1738593939@gmail.com>
2017-10-27 16:38:45 -0400

    Changed notation in comments and fixed init

--- macaw/train_tracks/dehn_thurston/path_reduction.py
+++ macaw/train_tracks/dehn_thurston/path_reduction.py
@@ -6,7 +6,7 @@ from numpy_list import NumpyList
 
 # See the doctests of the branch_encoding() method in dehn_thurston_tt2.py for
 # examples of the modified encoding conventions. The vertices are assumed to
-# have number at least 2, this is how you can distinguishthose elements of the encoding from LEFT, RIGHT, FORWARD, BACKWARD that are assigned to 0, 1 and -1.
+# have number at least 2, this is how you can distinguish those elements of the encoding from LEFT, RIGHT, FORWARD, BACKWARD that are assigned to 0, 1 and -1.
 
 # Teardrops have length 4 pieces of data between vertices
 # Pants curves and arcs have 2 pieces of data. The way to tell them apart is that pants curves use BACKWARD and FORWARD, assigned to -1 and 1, so there is always one entry that is -1. For arcs however, the two entries are LEFT or RIGHT, which are assigned to 0 and 1, respectively.
@@ -25,8 +25,6 @@ class PathReduction(object):
     (-) -> going through a negative gate
     (++) -> going around boundary along pants curve and returning
     (--) -> going around boundary against pants curve and returning
-    (C) -> a teardrop around hole clockwise
-    (A) -> a teardrop around hole anticlockwise
 
     (L) -> teardrop or boundary starting from the left
     (R) -> teardrop or boundary starting from the right
@@ -43,7 +41,7 @@ class PathReduction(object):
     """
     #TODO concrete examples for documentation
     def __init__(self, path):
-        self.path = NumpyList(np.array(path))
+        self.path = NumpyList(np.array(path), np.array(len(path)), False)
 
     """Defines the representation of the object in print stream"""
     def __repr__(self):
