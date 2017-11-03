04a43aa16ee56e9fac6c94d8bf705eac2385eb6d
Kyle Xiao <kylepxiao@gmail.com>
2017-11-02 23:47:14 -0400

    Finished helper methods and cleaned code

--- macaw/train_tracks/dehn_thurston/path_reduction.py
+++ macaw/train_tracks/dehn_thurston/path_reduction.py
@@ -16,22 +16,21 @@ from numpy_list import NumpyList
 import numpy as np
 
 class PathReduction(object):
-    """An representation of topological paths that can be reduced
+    """
+    A representation of topological paths that can be reduced
 
     A path can be represented as a list of operators
 
     (1, 2, 3, ...) -> node that the path goes through
     (+) -> going through a positive gate
     (-) -> going through a negative gate
-    (++) -> going around boundary along pants curve and returning
-    (--) -> going around boundary against pants curve and returning
-
-    (L) -> teardrop or boundary starting from the left
-    (R) -> teardrop or boundary starting from the right
+    (L) -> going left around boundary and returning
+    (R) -> going right boundary against and returning
+    (LNR) -> teardrop or boundary starting from the left around curve N
+    (RNL) -> teardrop or boundary starting from the right around curve N
 
     INPUT:
-
-    - ``path`` -- a list of operators encoded as strings
+        - ``path`` -- a list of operators encoded as strings
 
     EXAMPLES:
         from macaw import PathReduction
@@ -43,179 +42,234 @@ class PathReduction(object):
     def __init__(self, path):
         self.path = NumpyList(np.array(path, dtype="S3"), np.array([len(path)]), False)
 
-    """Defines the representation of the object in print stream"""
+    """
+    Defines the representation of the object in print stream
+    """
     def __repr__(self):
         return "Path with form " + repr(self.path)
 
+    """
+    Returns a number which represents the type of element represented
+    0 -> curve
+    1 -> gate
+    2 -> curve boundary
+    3 -> teardrop
+
+    INPUT:
+        - ``s`` -- the input element as a string
+    """
     def __type(self, s):
         if s == "+" or s == "-":
             return 1
-        elif s == "++" or s == "--":
-            return 2
         elif s == "L" or s == "R":
+            return 2
+        elif "L" in s and "R" in s:
             return 3
         else:
             return 0
+
+    """
+    ------------
+    NOTE: THIS FUNCTION IS DEPRECATED
 
     def __matches_six(self, subpath):
         if self.__type(subpath[5]) == 3:
             subpath.reverse()
         return subpath[0] == subpath[4] and self.__type(subpath[0]) == 0 and self.__type(subpath[7]) == 0 and subpath[1] == subpath[3] and subpath[3] == subpath[5] and self.__type(subpath[1]) == 1 and self.__type(subpath[2]) == 3 and self.__type(subpath[6]) == 1
+    ------------
+    """
 
-    """Reduces illegal path with redundant straight-path
+    """
+    Removes illegal path with redundant straight-path
 
     INPUT:
+        - ``start`` -- the starting index inclusive of illegal portion
+        - ``end`` -- the ending index inclusive of illegal portion
 
-    - ``start`` -- the starting index inclusive of illegal portion
-    - ``end`` -- the ending index exclusive of illegal portion
+    EXAMPLE
+        self.path = [c1, g1, g2, c2, g2, g1, c1]
+        self.__two(0, 6)
+        print(self)
+        >> Path with form []
     """
     def __one(self, start, end):
-        self.path = self.path[:start] + self.path[end:] #splices out portion that was identified as backtracing
+        s = np.sign(end - start)
+        #splices out portion that was identified as backtracing
+        if s > 0:
+            self.path.delete(slice(start, end + 1))
+        else:
+            self.path.delete(slice(end, start + 1))
 
+    """
+    Reduces illegal path that goes from starting point to another point and then circles around
+    the curve. Ouputs a teardrop around the destination curve
+
+    EXAMPLE
+        self.path = [c1, g1, g2, c2, t2, c2, g2, g1, c1]
+        self.__two(0, 8)
+        print(self)
+        >> Path with form [c1, T1, c1]
+    """
     def __two(self, start, end):
-        """
-            curves involved are in cyclic order
-            Takes two arrays as input
-
-            EXAMPLE
-                self.path = [c1, g1, g2, c2, t2, c2, g2, g1, c1]
-                self.__two(0, 9)
-                print(self)
-                >> Path with form [c1, T1, c1]
-
-        """
+        #assign respective values as outlined in schema
+        s = np.sign(end - start)
         c1 = self.path[start]
-        t2 = self.path[start + 4]
-        T1 = 'RL' if t2 == 'R' else 'LR'
-        self.path[start:end] = []
+        t2 = self.path[start + s * 4]
+        T1 = 'R' + c2 + 'L' if t2 == 'R' else 'L' + c2 + 'R'
         opath = [c1, T1, c1]
-        self.path[start:start] = opath
+        #delete illegal portion and insert edited path
+        if s > 0:
+            self.path.delete(slice(start, end + 1))
+            self.path.insert(start, opath)
+        else:
+            self.path.delete(slice(end, start + 1))
+            self.path.insert(end, opath)
 
+    """
+    Reduces illegal path beginning from starting point, going to the cyclic predecessors,
+    circles around that point, and then returns
+
+    INPUT:
+    - ``start`` -- the starting index inclusive of the illegal move
+    - ``end`` -- the ending index inclusive of the illegal move
+
+    EXAMPLE
+        self.path = [c1, g1, g2, c2, t2, c2, g2, g1, c1]
+        self.__three(0, 9)
+        print(self)
+        >> Path with form [c1, t1, c1, T1, c1]
+    """
     def __three(self, start, end):
-        """
-        Reduces illegal path beginning from starting point, going to the cyclic predecessors,
-        circles around that point, and then returns
-
-        INPUT:
-        - ``start`` -- the starting index inclusive of the illegal move
-        - ``end`` -- the ending index inclusive of the illegal move
-
-        EXAMPLE
-            self.path = [c1, g1, g2, c2, t2, c2, g2, g1, c1]
-            self.__three(0, 9)
-            print(self)
-            >> Path with form [c1, t1, c1, T1, c1]
-        """
+        #assign respective values as outlined in schema
         s = np.sign(end - start)
         c1 = self.path[start]
         c2 = self.path[start + s * 3]
         t2 = self.path[start + s * 4]
         t1 = 'L' if t2 == 'R' else 'R'
         T1 = 'L' + str(6 - int(c1) - int(c2)) + 'R' if t2 == 'R' else 'R' + str(6 - int(c1) - int(c2)) + 'L'
-        self.path.delete(slice(start, end))
+        #delete illegal portion and insert edited path
         opath = [c1, t1, c1, T1, c1]
-        self.path.insert(start, opath)
+        if s > 0:
+            self.path.delete(slice(start, end + 1))
+            self.path.insert(start, opath)
+        else:
+            self.path.delete(slice(end, start + 1))
+            self.path.insert(end, opath)
 
+    """
+    Reduces illegal path beginning from a starting point, going to the point to left that a teardrop
+    cannot form around, circles around that point, and goes back to the starting point.
 
-    def __four(self, start, end):
-        """
-        Reduces illegal path beginning from a starting point, going to the point to left that a teardrop
-        cannot form around, circles around that point, and goes back to the starting point.
+    TODO: Calculate orientation in a separate function? Need orientation for the pants curve
+        we are performing the reduction on. This should come from a PantsDecomposition object.
 
-        TODO: Calculate orientation in a separate function? Need orientation for the pants curve
-            we are performing the reduction on. This should come from a PantsDecomposition object.
+    INPUT:
+    - ``start`` -- the starting index inclusive of the illegal move
+    - ``end`` -- the ending index inclusive of the illegal move
 
-        INPUT:
-        - ``start`` -- the starting index inclusive of the illegal move
-        - ``end`` -- the ending index exclusive of the illegal move
-        - ``orientation`` -- string representation ("CC" or "L") of the orientation of the pants curve
-                            that the illegal path is starting from
-
-        EXAMPLE
-            self.path = [c1, g1, g2, c2, t2, c2, g2, g1, c3]
-            self.__four(0, 9)
-            print(self)
-            >> Path with form [c1, t1, c1, g1, g2, c3, t3, c3]
-        """
+    EXAMPLE
+        self.path = [c1, g1, g2, c2, t2, c2, g2, g1, c3] #TODO should the last g1 be g3 since you are going to c3?
+        self.__four(0, 8)
+        print(self)
+        >> Path with form [c1, t1, c1, g1, g2, c3, t3, c3]
+    """
+    def four(self, start, end):
+        #assign respective values as outlined in schema
+        s = np.sign(end - start)
         c1 = self.path[start]
-        g1 = self.path[start + 1]
-        c3 = self.path[end - 1]
-        g2 = self.path[start + 2]
-        t2 = self.path[start + 4]
+        g1 = self.path[start + s * 1]
+        g2 = self.path[start + s * 2]
+        t2 = self.path[start + s * 4]
+        c3 = self.path[start + s * 8]
         t1 = 'L' if t2 == 'R' else 'R'
         t3 = 'L' if t2 == 'R' else 'R'
-        self.path[start:end] = []
+        #delete illegal portion and insert edited path
         opath = [c1, t1, c1, g1, g2, c3, t3, c3]
-        self.path[start:start] = opath
+        if s > 0:
+            self.path.delete(slice(start, end + 1))
+            self.path.insert(start, opath)
+        else:
+            self.path.delete(slice(end, start + 1))
+            self.path.insert(end, opath)
 
-    def __five(self, start, end):
-        """
-        Reduces illegal path beginning from starting point, going to the point to the right that a teardrop
-        can form around, circles around that point, makes a teardrop around the third point from the second
-        point, and ends back at the second point.
+    """
+    Reduces illegal path beginning from starting point, going to the point to the right that a teardrop
+    can form around, circles around that point, makes a teardrop around the third point from the second
+    point, and ends back at the second point.
 
-        INPUT:
-        - ``start`` -- the starting index inclusive of the illegal move
-        - ``end`` -- the ending index exclusive of the illegal move
-        - ``orientation`` -- string representation ("CC" or "L") of the orientation of the pants curve
-                            that the illegal path is starting from
+    INPUT:
+    - ``start`` -- the starting index inclusive of the illegal move
+    - ``end`` -- the ending index exclusive of the illegal move
 
-        EXAMPLE
-            self.path = [c1, g1, g2, c2, t2, c2, T2, c2]
-            self.__five(0, 8)
-            print(self)
-            >> Path with form [c1, t1, c1, g1, g2, c2]
-        """
+    EXAMPLE
+        self.path = [c1, g1, g2, c2, t2, c2, T2, c2]
+        self.__five(0, 8)
+        print(self)
+        >> Path with form [c1, t1, c1, g1, g2, c2]
+    """
+    def five(self, start, end):
+        #assign respective values as outlined in schema
+        s = np.sign(end - start)
         c1 = self.path[start]
-        t2 = self.path[start + 4]
+        t2 = self.path[start + s * 4]
         t1 = 'L' if t2 == 'R' else 'R'
-        c2 = self.path[start + 3]
-        g1 = self.path[start + 1]
-        g2 = self.path[start + 2]
-        self.path[start:end] = []
+        c2 = self.path[start + s * 3]
+        g1 = self.path[start + s * 1]
+        g2 = self.path[start + s * 2]
+        #delete illegal portion and insert edited path
         opath = [c1, t1, c1, g1, g2, c2]
-        self.path[start:start] = opath
+        if s > 0:
+            self.path.delete(slice(start, end + 1))
+            self.path.insert(start, opath)
+        else:
+            self.path.delete(slice(end, start + 1))
+            self.path.insert(end, opath)
 
-    """Reduces illegal path with teardrop and straight-path assuming an 8-character start to end encoding
+    """
+    Reduces illegal path with teardrop and straight-path assuming an 8-character start to end encoding
 
     INPUT:
 
     - ``start`` -- the starting index inclusive of illegal portion
-    - ``end`` -- the ending index exclusive of illegal portion
+    - ``end`` -- the ending index inclusive of illegal portion
 
     EXAMPLE
-    self.path = ['1', '+', 'C', '+', '1', '+', '-', '2']
-    self.__six(0)
+    self.path = [c1, g1, g2, c2, T2, c2]
+    self.__six(0, 5)
     print(self)
-    >> Path with form ['1', '+', '-', '2', '--', '2']
+    >> Path with form [c1, t1, c1, g1, g2, c2] (assuming T2 contains c1)
+    >> Path with form [c1, t1, c1, g1, g2, c2, t2, c2] (assuming T2 contains c3)
     """
-    def __six(self, start):
-        reverse = self.__type(self.path[start + 5]) == 3
-        if reverse == True:
-            s1 = self.path[start+1]
-            t1 = self.path[start+5]
-            self.path[start+1:start+6] = []
-            self.path.insert(start+1, s1)
-            self.path.insert(start+1, self.path[start])
-            if t1 == "L":
-                self.path.insert(start+1, "--")
-            else:
-                self.path.insert(start+1, "++")
+    def __six(self, start, end):
+        #assign respective values as outlined in schema
+        s = np.sign(end - start)
+        c1 = self.path[start]
+        g1 = self.path[start + s * 1]
+        g2 = self.path[start + s * 2]
+        c2 = self.path[start + s * 3]
+        T2 = self.path[start + s * 4]
+        if T2[1] == c1:
+            t1 = 'L' if T2.startswith('R') else 'R'
+            opath = [c1, t1, c1, g1, g2, c2]
+        else: #T2 contains c3
+            if (int(c1) + 1) % 3 == int(c2): #c2 is cyclic successor
+                t1 = 'R'
+                t2 = 'R'
+            else: #c2 is cyclic predecessors
+                t1 = 'L'
+                t2 = 'L'
+            opath = [c1, t1, c1, g1, g2, c2, t2, c2]
+        if s > 0:
+            self.path.delete(slice(start, end + 1))
+            self.path.insert(start, opath)
         else:
-            s1 = self.path[start+6]
-            t1 = self.path[start+2]
-            self.path[start+2:start+7] = []
-            if t1 == "L":
-                self.path.insert(start+2, "--")
-            else:
-                self.path.insert(start+2, "++")
-            self.path.insert(start+2, self.path[start+3])
-            self.path.insert(start+2, s1)
+            self.path.delete(slice(end, start + 1))
+            self.path.insert(end, opath)
 
-
-    """Main function that reduces paths
     """
-    #TODO concrete examples for documentation
+    Main function that reduces paths
+    """
+    #TODO concrete examples for documentation and begin working on illegal path checking
     def reduce(self):
         interval = 3 #during iteration, the interval determines the groupings in which the function will check for illegal paths
         #TODO adjust to account for sizes of illegal paths
